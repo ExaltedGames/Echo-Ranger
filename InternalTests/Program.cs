44278caffba524dcb320a95730e4de45a03861d2
Sprocket <lpolar45@gmail.com>
2023-12-11 23:25:00 -0500

    we a bit silly

--- InternalTests/Program.cs
+++ InternalTests/Program.cs
@@ -5,7 +5,7 @@ public static class Program
     public static void Main(string[] args)
     {
         Console.WriteLine("Running tests...");
-        //Tests.TestMoveLoading();    
+        Tests.TestMoveLoading();    
         Tests.TestBattle();
     }
 }
