178653bf688821bffdc1622d7b51b4654c14ff65
buckly90 <kormur@protonmail.com>
2026-01-09 17:57:13 -0500

    Corrected multiplicative and additive multipliers applying by default

--- HackmonInternals/Models/Modifier.cs
+++ HackmonInternals/Models/Modifier.cs
@@ -6,5 +6,8 @@ namespace HackmonInternals.Models;
 public class Modifier
 {
 	public int BaseAdditiveBonus { get; set; } = 0;
-	public double Multiplier { get; set; } = 1;
+	/// <summary>
+	/// Negative mods need to be negative numbers and vice versa. 0 = no change.
+	/// </summary>
+	public double Multiplier { get; set; } = 0;
 }
