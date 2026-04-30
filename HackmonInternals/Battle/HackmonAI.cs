fc7d6ff88dd671921bd61cfe45a61caf74ba4f91
buckly90 <kormur@protonmail.com>
2026-04-30 01:56:53 -0400

    Added check that prevents AI from using moves it does not have enough stamina for

--- HackmonInternals/Battle/HackmonAI.cs
+++ HackmonInternals/Battle/HackmonAI.cs
@@ -10,14 +10,21 @@ public class HackmonAI : IBattleAI
 		if (actor is not HackmonInstance unit)
 			throw new Exception("Used outside of intended context.");
 
+		var validMoves = unit.KnownMoves.Where(moveCandidate =>
+		{
+			var moveEquals = HackmonManager.MoveRegistry[moveCandidate];
+			Console.WriteLine($"{moveCandidate} chosen that subtracts {moveEquals.StaminaCost} from unit's {unit.Stamina}");
+			return moveEquals.StaminaCost <= unit.Stamina;
+		}).ToList();
+		
 		Random rand = new();
 		var maxMoves = 6;
-		if (unit.KnownMoves.Count < 6)
-			maxMoves = unit.KnownMoves.Count;
+		if (validMoves.Count < 6)
+			maxMoves = validMoves.Count;
 
 		var choice = rand.Next(maxMoves);
 		Console.WriteLine($"Selected {choice} from {maxMoves} total moves.");
-		var moveId = unit.KnownMoves[choice];
+		var moveId = validMoves[choice];
 		var move = HackmonManager.MoveRegistry[moveId];
 		var target = BattleManager.PlayerTeam[0];
 
