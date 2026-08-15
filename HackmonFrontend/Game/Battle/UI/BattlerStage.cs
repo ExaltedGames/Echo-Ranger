ae7f683249815302d9e8a398205b26271b598b8e
buckly90 <kormur@protonmail.com>
2026-08-15 17:06:13 -0400

    Implemented programatic loading of idle animation

--- HackmonFrontend/Game/Battle/UI/BattlerStage.cs
+++ HackmonFrontend/Game/Battle/UI/BattlerStage.cs
@@ -12,9 +12,23 @@ public partial class BattlerStage : Node2D
 	public void LoadHackmon(string hackmonName, bool doFlip = false)
 	{
 		_hackmonSprite.SpriteFrames.ClearAll();
-		var newSprite = ResourceLoader.Load<Texture2D>($"Assets/BattleSprites/{hackmonName}/{hackmonName}.png");
+		_hackmonSprite.SpriteFrames.AddAnimation("idle");
+		var newSprite = ResourceLoader.Load<Texture2D>($"Assets/BattleSprites/{hackmonName}/{hackmonName}_Idle.png");
+		var frameEdge = 96;
+		for (var v = 0; v < 5; v++)
+		{
+			for (var h = 0; h < 6; h++)
+			{
+				var frame = new Rect2(h * frameEdge, v * frameEdge, frameEdge, frameEdge);
+				var atlasTexture = new AtlasTexture();
+				atlasTexture.Atlas = newSprite;
+				atlasTexture.Region = frame;
+				_hackmonSprite.SpriteFrames.AddFrame("idle", atlasTexture);
+			}
+		}
 		_hackmonSprite.FlipH = doFlip;
-		_hackmonSprite.SpriteFrames.AddFrame("default", newSprite);
+		_hackmonSprite.SpriteFrames.SetAnimationSpeed("idle", 30);
+		_hackmonSprite.Play("idle");
 	}
 
 	public override void _Process(double delta)
